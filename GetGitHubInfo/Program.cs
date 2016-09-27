35bd22b424d8a3f88a4581b3523f830523fb1632
xbz <609072037@QQ.com>
2016-09-27 23:26:06 +0800

    初始化代码管理

---
+++ GetGitHubInfo/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace GetGitHubInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("当前I开始发送请求...，请稍候!:" + 1);
+            Random r = new Random();
+            string strPageSize = Save("1","android", true);
+            if (strPageSize != "")
+            {
+                int pageSize = Convert.ToInt32(strPageSize);
+                int ran = r.Next(2, 8);
+                int sleepTime = 1000 * 60 * ran;
+                for (int i = 2; i <= pageSize; i++)
+                {
+                    Console.WriteLine("当前I睡眠中...，请稍候!:" + i);
+                    
+                    StopWatch sw = new StopWatch(Console.CursorLeft, Console.CursorTop, sleepTime);
+                    sw.Start();
+                    System.Threading.Thread.Sleep(sleepTime);
+                    sw.finsh();
+                    Console.WriteLine();
+                    Console.WriteLine("当前I开始发送请求...，请稍候!:" + i);
+                    Save(i + "", "android");
+                    Console.WriteLine("当前I请求完成!:" + i);
+                }
+                Console.ReadLine();
+            }
+        }
+
+        public static string Save(string index, string name, bool isOne = false)
+        {
+            //第一步声明HtmlAgilityPack.HtmlDocument实例
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            //获取Html页面代码
+            //string html = GetHttp("5","android");
+            Dictionary<string, string> result = HttpGET(index, name);
+            string html = result["result"];
+            Console.WriteLine("html:" + html);
+            //第二步加载html文档
+            doc.LoadHtml(html);
+            //获取总页数
+            string strPageSize = "";
+            if (isOne)
+            {
+                HtmlAgilityPack.HtmlNodeCollection htmlnode = doc.DocumentNode.SelectNodes("//div[@class='paginate-container']/div[@class='pagination']/a");
+                strPageSize = htmlnode[htmlnode.Count - 2].InnerText;
+                Console.WriteLine("总页数是：" + strPageSize);
+                Console.WriteLine();
+                Console.WriteLine();
+                htmlnode = null;
+            }
+            //获取所有板块的a标签
+            HtmlAgilityPack.HtmlNodeCollection collection = doc.DocumentNode.SelectNodes("//ul[@class='repo-list js-repo-list']/li");
+            if (collection == null)
+            {
+                return "";
+            }
+            foreach (HtmlAgilityPack.HtmlNode item in collection)
+            {
+                string strURL = "";
+                HtmlAgilityPack.HtmlNode mItem;
+                mItem = item.SelectSingleNode("//h3[@class='repo-list-name']/a");
+                strURL = "https://github.com" + mItem.Attributes["href"].Value;
+
+                if (RedisCacheHelper.Exists(strURL))
+                {
+                    Dictionary<string, string> mResult = RedisCacheHelper.Get<Dictionary<string, string>>(strURL);
+                    Console.WriteLine("重新取出的数据...");
+                    Console.WriteLine("名字是：" + mResult["name"]);
+                    Console.WriteLine("地址是：" + mResult["url"]);
+                    Console.WriteLine("说明是：" + mResult["detailed"]);
+                    Console.WriteLine("更新时间是：" + mResult["updateTime"]);
+                    Console.WriteLine("Stargazers是：" + mResult["stargazers"]);
+                    Console.WriteLine("Forks是：" + mResult["forks"]);
+                    Console.WriteLine();
+                    item.RemoveAll();
+                    strURL = "";
+                    //同一路径存在表示当前项目已爬过，跳过即可
+                    continue;
+                }
+                Dictionary<string, string> mDic = new Dictionary<string, string>();
+                mDic.Add("name", mItem.InnerText.Replace("/n", "").Trim());
+                mDic.Add("url", strURL);
+                Console.WriteLine("名字是：" + mItem.InnerText.Replace("/n", "").Trim());
+                Console.WriteLine("地址是：" + strURL);
+                mItem.RemoveAll();
+                mItem = item.SelectSingleNode("//p[@class='repo-list-description']");
+                mDic.Add("detailed", mItem.InnerText == null ? "" : mItem.InnerText.Replace("/n", "").Trim());
+                Console.WriteLine("说明是：" + mItem.InnerText == null ? "" : mItem.InnerText.Replace("/n", "").Trim());
+                mItem.RemoveAll();
+                mItem = item.SelectSingleNode("//p[@class='repo-list-meta']");
+                mDic.Add("updateTime", mItem.InnerText == null ? "" : mItem.InnerText.Replace("/n", "").Trim());
+                Console.WriteLine("更新时间是：" + mItem.InnerText.Replace("/n", "").Trim());
+                mItem = item.SelectSingleNode("//div[@class='repo-list-stats']/a[@aria-label='Stargazers']");
+                mDic.Add("stargazers", mItem.InnerText == null ? "" : mItem.InnerText.Replace("/n", "").Trim());
+                Console.WriteLine("Stargazers是：" + mItem.InnerText.Replace("/n", "").Trim());
+                mItem = item.SelectSingleNode("//div[@class='repo-list-stats']/a[@aria-label='Forks']");
+                mDic.Add("forks", mItem.InnerText == null ? "" : mItem.InnerText.Replace("/n", "").Trim());
+                Console.WriteLine("Forks是：" + mItem.InnerText.Replace("/n", "").Trim());
+                Console.WriteLine();
+                RedisCacheHelper.Add<Dictionary<string, string>>(strURL, mDic);
+                item.RemoveAll();
+                mDic.Clear();
+                strURL = "";
+            }
+            //Console.ReadLine();
+            doc = null;
+            result = null;
+            collection = null;
+            return strPageSize;
+        }
+
+        public static string GetHttp(string index, string name)
+        {
+            string str = "https://github.com/search?o=desc&p=" + index + "&q=" + name + "&ref=searchresults&s=stars&type=Repositories&utf8=%E2%9C%93";
+            WebClient MyWebClient = new WebClient();
+            MyWebClient.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
+
+            Byte[] pageData = MyWebClient.DownloadData(str); //从指定网站下载数据
+            string pageHtml = Encoding.UTF8.GetString(pageData); //如果获取网站页面采用的是UTF-8，则使用这句
+            return pageHtml;
+        }
+        #region GET请求
+        /// <summary>
+        /// 发送请求 GET
+        /// </summary>
+        /// <param name="Url">请求URL</param>
+        /// <param name="ContentType">内容类型  可以为null</param>
+        /// <param name="Headers">头文件 可以为null </param>
+        /// <param name="PostData">post参数内容 可以为null </param>
+        /// <returns></returns>
+        public static Dictionary<string, string> HttpGET(string index, string name)
+        {
+            string str = "https://github.com/search?o=desc&p=" + index + "&q=" + name + "&ref=searchresults&s=stars&type=Repositories&utf8=%E2%9C%93";
+            return SendRequest(str, "GET", null, null, null);
+        }
+        #endregion
+        #region 公共方法
+        /// <summary>
+        /// 发送请求
+        /// </summary>
+        /// <param name="Url">请求URL</param>
+        /// <param name="ContentType">内容类型  可以为null</param>
+        /// <param name="Headers">头文件 可以为null </param>
+        /// <param name="PostData">post参数内容 可以为null </param>
+        /// <returns>code and result</returns>
+        public static Dictionary<string, string> SendRequest(string Url, string Method, string ContentType, Dictionary<string, string> Headers, string PostData)
+        {
+            Dictionary<string, string> responseResult = new Dictionary<string, string>();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            //设置请求模式
+            request.Method = Method;
+
+            //设置内容类型
+            if (ContentType != null)
+            {
+                request.ContentType = ContentType;
+            }
+
+            //设置Header
+            if (Headers != null)
+            {
+                foreach (KeyValuePair<string, string> para in Headers)
+                {
+                    request.Headers.Add(para.Key, para.Value);
+                }
+            }
+
+            //只有参数不为空的时候才发送
+            if (PostData != null)
+            {
+                //将字符串转换为字节
+                byte[] postdata = Encoding.UTF8.GetBytes(PostData);
+                request.ContentLength = postdata.Length;
+
+                using (Stream reqStream = request.GetRequestStream())
+                {
+                    reqStream.Write(postdata, 0, postdata.Length);
+                    reqStream.Close();
+                }
+            }
+
+            string result = "";
+            int statusCode = 200;
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+                statusCode = (int)response.StatusCode;
+                result = myStreamReader.ReadToEnd();
+                myStreamReader.Close();
+                myResponseStream.Close();
+            }
+            catch (WebException ex)
+            {
+                HttpWebResponse res = ex.Response as HttpWebResponse;
+                Stream myResponseStream = res.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+                statusCode = (int)res.StatusCode;
+                result = myStreamReader.ReadToEnd();
+                myStreamReader.Close();
+                myResponseStream.Close();
+            }
+            responseResult.Add("code", statusCode + "");
+            responseResult.Add("result", result);
+            return responseResult;
+        }
+        #endregion
+
+
+
+        class StopWatch
+        {
+            private int Interval = 1000;              //时间间隔，单位毫秒
+            private int Time = 0;                        //所显示的时间
+            private int left = 0;
+            private int top = 0;
+            private Thread timer;
+
+            public StopWatch() { }
+            public StopWatch(int left, int top,int time)
+            {
+                this.left = left;
+                this.top = top;
+                Time = time;
+            }
+
+            public void Start()
+            {
+                timer = new Thread(new ThreadStart(Timer));  //新建一个线程，该线程调用Timer()
+                timer.Start();                               //启动线程
+                Console.CursorVisible = false;   //隐藏光标
+                
+            }
+            private void Timer()
+            {
+                while (true)
+                {
+                    Display();                               //显示秒表计数
+                    Thread.Sleep(Interval);         //等待1秒后再执行Timer()刷新计数
+                    Time = Time - 1000;                                 //秒数加1
+                }
+            }
+            private void Display()
+            {
+                Console.SetCursorPosition(left,top);
+                Console.Write("剩余:[" + Time/1000 + "]秒");
+            }
+            public void finsh()
+            {
+                if(timer!=null)
+                    timer.Abort();                              //终止线程,用于停止秒表
+                Console.SetCursorPosition(left, top);
+                Console.Write("剩余:[0]秒");
+            }
+        }
+    }
+}
---
+++ GetGitHubInfo/RedisCacheHelper.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Configuration;
+using ServiceStack.Redis;
+
+namespace GetGitHubInfo
+{
+    public class RedisCacheHelper
+    {
+        private static string strErrorInfo = "{0}:{1}发生异常!key={2},异常信息={3}";
+        private static readonly PooledRedisClientManager pool = null;
+        private static readonly string[] redisHosts = null;
+        public static int RedisMaxReadPool = int.Parse(ConfigurationManager.AppSettings["redis_max_read_pool"]);
+        public static int RedisMaxWritePool = int.Parse(ConfigurationManager.AppSettings["redis_max_write_pool"]);
+
+        static RedisCacheHelper()
+        {
+            var redisHostStr = ConfigurationManager.AppSettings["redis_server_session"];
+
+            if (!string.IsNullOrEmpty(redisHostStr))
+            {
+                redisHosts = redisHostStr.Split(',');
+
+                if (redisHosts.Length > 0)
+                {
+                    pool = new PooledRedisClientManager(redisHosts, redisHosts,
+                        new RedisClientManagerConfig()
+                        {
+                            MaxWritePoolSize = RedisMaxWritePool,
+                            MaxReadPoolSize = RedisMaxReadPool,
+                            AutoStart = true
+                        });
+                }
+            }
+        }
+
+        public static void Add<T>(string key, T value)
+        {
+            if (value == null)
+            {
+                return;
+            }
+            try
+            {
+                if (pool != null)
+                {
+                    using (var r = pool.GetClient())
+                    {
+                        if (r != null)
+                        {
+                            r.SendTimeout = 1000;
+                            r.Set(key, value);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = string.Format(strErrorInfo, "cache", "存储", key, ex.Message);
+            }
+
+        }
+
+        public static void Add<T>(string key, T value, DateTime expiry)
+        {
+            if (value == null)
+            {
+                return;
+            }
+
+            if (expiry <= DateTime.Now)
+            {
+                Remove(key);
+
+                return;
+            }
+
+            try
+            {
+                if (pool != null)
+                {
+                    using (var r = pool.GetClient())
+                    {
+                        if (r != null)
+                        {
+                            r.SendTimeout = 1000;
+                            r.Set(key, value, expiry - DateTime.Now);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = string.Format(strErrorInfo, "cache", "存储", key, ex.Message);
+            }
+
+        }
+
+        public static void Add<T>(string key, T value, TimeSpan slidingExpiration)
+        {
+            if (value == null)
+            {
+                return;
+            }
+
+            if (slidingExpiration.TotalSeconds <= 0)
+            {
+                Remove(key);
+
+                return;
+            }
+
+            try
+            {
+                if (pool != null)
+                {
+                    using (var r = pool.GetClient())
+                    {
+                        if (r != null)
+                        {
+                            r.SendTimeout = 1000;
+                            r.Set(key, value, slidingExpiration);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = string.Format(strErrorInfo, "cache", "存储", key, ex.Message);
+            }
+
+        }
+
+
+
+        public static T Get<T>(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return default(T);
+            }
+
+            T obj = default(T);
+
+            try
+            {
+                if (pool != null)
+                {
+                    using (var r = pool.GetClient())
+                    {
+                        if (r != null)
+                        {
+                            r.SendTimeout = 1000;
+                            obj = r.Get<T>(key);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = string.Format(strErrorInfo, "cache", "获取", key, ex.Message);
+            }
+
+
+            return obj;
+        }
+
+        public static void Remove(string key)
+        {
+            try
+            {
+                if (pool != null)
+                {
+                    using (var r = pool.GetClient())
+                    {
+                        if (r != null)
+                        {
+                            r.SendTimeout = 1000;
+                            r.Remove(key);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = string.Format(strErrorInfo, "cache", "删除", key, ex.Message);
+            }
+
+        }
+
+        public static bool Exists(string key)
+        {
+            try
+            {
+                if (pool != null)
+                {
+                    using (var r = pool.GetClient())
+                    {
+                        if (r != null)
+                        {
+                            r.SendTimeout = 1000;
+                            return r.ContainsKey(key);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = string.Format(strErrorInfo, "cache", "是否存在", key, ex.Message);
+            }
+
+            return false;
+        }
+    }
+}
